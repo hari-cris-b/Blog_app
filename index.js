162c70acb710704c5d674fc39c5fe0d3b5d5d553
hari-cris-b <harikrish120027@gmail.com>
2024-11-06 18:12:02 +0530

    style issue resolve2

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 // Set the views directory
 app.set('views', path.join(__dirname, 'views'));
 
